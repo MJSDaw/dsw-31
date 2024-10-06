b2adef4ea33f60493e46f12a582c5582379f8214
MJSDaw <mjsdaw@gmail.com>
2024-10-06 14:13:40 +0100

    Start development of listado page

---
+++ listado.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión de productos - listado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 id="titulo">Gestión de Productos</h1>
+    <table>
+        <tr>
+            <th>Detalle</th>
+            <th>Código</th>
+            <th>Nombre</th>
+            <th>Acciones</th>
+        </tr>
+        <tr>
+            <td>Detalle</td>
+            <td>3</td>
+            <td>Mouse nose que no se cuantos</td>
+            <td>Actualizar | Borrar</td>
+        </tr>
+        <tr>
+            <td>Detalle</td>
+            <td>3</td>
+            <td>Mouse nose que no se cuantos</td>
+            <td>Actualizar | Borrar</td>
+        </tr>
+        <tr>
+            <td>Detalle</td>
+            <td>3</td>
+            <td>Mouse nose que no se cuantos</td>
+            <td>Actualizar | Borrar</td>
+        </tr>
+    </table>
+</body>
+</html>
